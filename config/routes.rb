c8e854300eabd71b3152796d417157205d53f07a
Leticia <leticia@macbook-pro-60.home>
2018-02-18 19:37:39 -0300

    relation between friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   get 'my_portifolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
